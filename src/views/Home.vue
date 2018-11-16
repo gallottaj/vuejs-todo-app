38c2ad0e0734c46360acb2524090eb25d397180f
Joe Gallotta <joeygallotta@gmail.com>
2018-11-15 21:08:21 -0600

    create three tasks

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <p>{{ task1 }}</p>
+    <p>{{ task2 }}</p>
+    <p>{{ task3 }}</p>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      task1: "This is a new task",
+      task2: "This is a new task",
+      task3: "This is a new task"
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
+</script>
+
+
